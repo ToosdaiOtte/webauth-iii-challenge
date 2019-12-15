14a200c828420409fea7c9d6ef3e78846208fb59
Toosdai Otte <toosdai52Otte@gmail.com>
2019-12-15 14:47:16 -0600

    http request for login/register and get users working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,15 +1,63 @@
 const router = require('express').Router();
-const bcyrpt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const secrets = require('../config/secrets.js');
+const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
 
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error registering user',
+                err
+            })
+        })
 });
 
 router.post('/login', (req, res) => {
+    let { username, password } = req.body;
 
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                const token = genToken(user);
+
+                res.status(200).json({
+                    message: `Welcome ${user.username}`,
+                    token: token
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error occured while logging in',
+                err
+           })
+        });
 });
+
+function genToken(user) {
+    const payload = {
+        userid: user.id,
+        username: user.username
+    }
+
+    const options = {
+        expiresIn: '1d'
+    }
+
+    const token = jwt.sign(payload, secrets.jwtSecret, options)
+
+    return token;
+}
 
 module.exports = router;
